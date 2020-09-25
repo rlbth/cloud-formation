4b7432fe643974225f316579b32d6688c7592df5
Lakshmi Bhargavi Rangavajjula <43711076+rlbth@users.noreply.github.com>
2020-09-25 16:24:29 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -3,5 +3,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=eu-west-2 \
+--region=us-east-2 \
 --capabilities CAPABILITY_NAMED_IAM
